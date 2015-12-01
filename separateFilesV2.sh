e58e979d308a14b18b1f2ff678c6395cf95e422b
Zachariah Miller <zamiller@pdsf8.nersc.gov>
2015-12-01 08:21:16 -0800

    update separateFilesV2

--- separateFilesV2.sh
+++ separateFilesV2.sh
@@ -8,16 +8,16 @@ then
 fi
 
 i=1
-files=`find . -maxdepth 1 -name "*.root" -type f -mmin +6 -size +5800k | wc -l`
+files=`find . -maxdepth 1 -name "*.root" -type f -mmin +60 -size +5800k | wc -l`
 echo "Num. Files: $files"
 while [ $files -gt 0 ]
 do
   mkdir $1_set$i
-  find . -maxdepth 1 -name "*.root" -type f -mmin +6 -size +5800k | head -100 | xargs mv -t $1_set$i
+  find . -maxdepth 1 -name "*.root" -type f -mmin +60 -size +5800k | head -1000 | xargs mv -t $1_set$i
   cd $1_set$i
   hadd pythia_tree_$1_$i.root *.root
   cd ..
-  files=`find . -maxdepth 1 -name "*.root" -type f -mmin +6 -size +5800k | wc -l`
+  files=`find . -maxdepth 1 -name "*.root" -type f -mmin +60 -size +5800k | wc -l`
   echo "$files Remain"
   i=$[i+1]
 done
